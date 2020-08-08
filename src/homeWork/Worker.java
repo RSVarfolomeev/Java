bf7f3fe82441ba46b3d23616ba6d3940b67a6a19
RSVarfolomeev <hrom9090@mail.ru>
2020-08-08 21:52:51 +0300

    добаление 5 урока

--- src/homeWork/Worker.java
+++ src/homeWork/Worker.java
@@ -4,11 +4,14 @@ public class Worker {
     private String full_name;
     private int salary;
     private int age;
+    private static int id = 0;
+    public int vid;
 
     Worker(String full_name, int salary, int age){
         this.full_name = full_name;
         this.salary = salary;
         this.age = age;
+        this.vid = ++id;
     }
 
     public String getFull_name() {
